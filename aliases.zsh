b00990b2ef1474f20cbc02ae0678b355449fea73
Jacob Sawatzky <jsawatzky@outlook.com>
2021-05-31 16:00:49 -0400

    Update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,8 +1,15 @@
 alias refresh-shell="source ~/.zshrc"
-alias zsh-config="vim ~/.zshrc"
+alias zsh-config="$EDITOR ~/.zshrc"
 
 alias grep="egrep"
+alias ping="ping -c 5"
 
 alias gs="git status -suall"
 
-alias ghist="history | grep"
+alias hg="history | grep"
+alias ag="alias | grep"
+
+mdcd() {
+    mkdir -p $1
+    cd $1
+}
